9408a384d54913c10b1f7a3ec39ca4dc9fd103c1
Cristian <Cristian>
2022-04-27 10:42:45 +0300

    ok

--- Shared/ViewModel.swift
+++ Shared/ViewModel.swift
@@ -13,7 +13,6 @@ class ViewModel: ObservableObject {
     private let timer = BackgroundTimer(DispatchQueue(label: "BackgroundTimer", attributes: .concurrent))
     
     func timer(_ action:BackgroundTimer.Action) {
-        print(#function, action)
         switch action {
             case .start: timer.perform(.start)
             case .pause: timer.perform(.pause)
