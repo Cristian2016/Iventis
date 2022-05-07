d650cb0d2a8cee0532384b6a641d3c2c18252064
Cristian <Cristian>
2022-05-07 09:28:36 +0300

    ok

--- DetailView.swift
+++ DetailView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct DetailView: View {
+    @StateObject var tabWrapper = SellectedTabWrapper()
+    @FetchRequest var sessions:FetchedResults<Session>
+    let rank:Int?
+    
+    init(_ rank:Int?) {
+        let predicate:NSPredicate?
+        if let rank = rank { predicate = NSPredicate(format: "bubble.rank == %i", rank)
+        } else { predicate = nil }
+        
+        let descriptor = NSSortDescriptor(key: "created", ascending: false)
+        _sessions = FetchRequest(entity: Session.entity(), sortDescriptors: [descriptor], predicate: predicate, animation: .easeInOut)
+        self.rank = rank
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            TopDetailView(rank)
+                .frame(width: UIScreen.size.width * 0.96, height: 140)
+            BottomDetailView(rank)
+                .frame(width: UIScreen.size.width * 0.96, height: 410)
+        }
+        .ignoresSafeArea()
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(4)
     }
 }
