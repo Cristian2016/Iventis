9dfd3237cb2466f4ed305f2903d9a527aa1b2342
Cristian <Cristian>
2022-05-14 01:17:12 +0300

    ok

--- Shared/Core Data/Bubble.swift
+++ Shared/Core Data/Bubble.swift
@@ -39,7 +39,7 @@ public class Bubble: NSManagedObject {
     var lastPair:Pair? { (lastSession?.pairs?.array as? [Pair])?.last }
     
     // MARK: -
-    ///what bubbleCell displays. ex: "12"hr "34"min "59"sec
+    ///time components that bubbleCell displays. ex: "12"hr "34"min "59"sec
     @Published var bubbleCell_Components
     = Float.TimeComponentsAsStrings(hr: "0", min: "0", sec: "0", cents: "00")
     { willSet { self.objectWillChange.send() }}
