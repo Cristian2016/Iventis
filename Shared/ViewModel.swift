c7fcd6f7cd8199c576189754100549d7c207b1e1
Cristian <Cristian>
2022-05-19 00:54:48 +0300

    ok

--- Shared/ViewModel.swift
+++ Shared/ViewModel.swift
@@ -246,7 +246,7 @@ class ViewModel: ObservableObject {
 
 extension ViewModel {
     ///all old notes sorted by Date
-    static func allOldBubbleNotes() -> [OldBubbleNote] {
+    func allOldBubbleNotes() -> [OldBubbleNote] {
         let request = OldBubbleNote.fetchRequest()
         request.sortDescriptors = [
             NSSortDescriptor(key: "created", ascending: false)
