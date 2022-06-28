2fb22873db7d43f1a20c690e6a5b3dbbc73f3063
Cristian <Cristian>
2022-06-28 12:23:41 +0300

    ok

--- DeleteRow_InfoView.swift
+++ DeleteRow_InfoView.swift
@@ -12,10 +12,13 @@ struct DeleteRow_InfoView: View {
         HStack {
             Spacer()
             VStack (alignment: .leading) {
+                Divider()
+                    .frame(maxWidth: .infinity)
                 Text("\(Image(systemName: "trash")) Delete Row")
                 Text("\(Image(systemName: "arrow.backward.circle.fill")) Swipe Left")
                     .foregroundColor(.lightGray)
             }
+            .fixedSize(horizontal: true, vertical: false)
             Spacer()
         }
     }
