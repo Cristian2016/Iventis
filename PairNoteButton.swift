645e1dc3deec99dd7bce2621cb6a205b003ffd5a
Cristian <Cristian>
2022-07-05 19:14:40 +0300

    ok

--- PairNoteButton.swift
+++ PairNoteButton.swift
@@ -10,7 +10,7 @@
 import SwiftUI
 
 struct PairNoteButton: View {
-    @ObservedObject var pair: Pair
+    var pair: Pair
     @State var offsetX = CGFloat.zero
     
     var triggerPairDeleteAction:Bool { abs(offsetX) > 180 }
