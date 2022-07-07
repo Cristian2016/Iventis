da80df02bbb0bd085e092f12d77323974a416d87
Cristian <Cristian>
2022-07-07 12:56:21 +0300

    ok

--- Shared/BubbleNote.swift
+++ Shared/BubbleNote.swift
@@ -24,7 +24,6 @@ struct BubbleNote: View {
                 calendarSymbol
                 stickyNoteTextView
             }
-//            .opacity(bubble.note_.isEmpty ? 0 : 1)
             .foregroundColor(.label)
             .background(background)
             .cornerRadius(cornerRadius)
