4f4897c032d77bafe36854e8fc4a0caa6ba1ce65
Cristian <Cristian>
2023-01-31 22:01:54 +0200

    ok

--- BubbleDeleteAlert.swift
+++ BubbleDeleteAlert.swift
@@ -53,7 +53,10 @@ struct BubbleDeleteAlert: View {
                         withAnimation {
                             viewModel.delete(bubble)
                             viewModel.deleteAction_bRank = nil
-                            viewModel.fiveSeconds_bRank = nil //ViewModel 1
+                            
+                            if viewModel.fiveSeconds_bRank == bubble.rank {
+                                viewModel.fiveSeconds_bRank = nil
+                            }//ViewModel 1
                         }
                     }
                 if !bubble.sessions_.isEmpty {
