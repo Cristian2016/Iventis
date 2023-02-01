e093bc6e73094ae779456ebc32ce5949b12f79e1
Cristian <Cristian>
2023-02-01 13:59:22 +0200

    ok

--- Shared/BubbleList.swift
+++ Shared/BubbleList.swift
@@ -80,7 +80,7 @@ struct BubbleList: View {
     
     private var showAllButton:some View {
         let count = Secretary.shared.unpinnedBubblesCount
-        return Text("\(Image(systemName: "eye")) Unpinned \(count)")
+        return Text("\(Image(systemName: "eye")) \(count) Unpinned")
             .listRowSeparator(.hidden)
             .font(.footnote)
             .foregroundColor(.secondary)
