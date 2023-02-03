25bc70c1ba177659fdf793c1e06c44b102175d69
Cristian <Cristian>
2023-02-03 12:31:55 +0200

    ok

--- DurationPickerView.swift
+++ DurationPickerView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct DurationPickerView: View {
+    @EnvironmentObject private var viewModel:ViewModel
+    
     @State private var hr:Int = 0
     @State private var min:Int = 0
     @State private var sec:Int = 0
@@ -22,57 +24,73 @@ struct DurationPickerView: View {
     private let digits = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "00", "0", "✕"]
     
     var body: some View {
-        ZStack {
-            Color.background
-            VStack {
-                let font = Font.system(size: 60, weight: .regular, design: .rounded)
-                let white = Color.white
-                
-                Rectangle()
-                    .fill(.background)
-                    .frame(height: 60)
-                    .overlay {
-                        Text("\(hr)Hr \(min)Min \(sec)Sec")
-                            .font(.largeTitle)
+        HStack {
+            ZStack {
+                Color.background
+                VStack {
+                    let font = Font.system(size: 60, weight: .regular, design: .rounded)
+                    let white = Color.white
+                    
+                    Rectangle()
+                        .fill(.background)
+                        .frame(height: 60)
+                        .overlay {
+                            Text("\(hr)Hr \(min)Min \(sec)Sec")
+                                .font(.largeTitle)
+                        }
+                    
+                    HStack {
+                        Picker(selection: $hr) {
+                            ForEach(hrValues, id: \.self) { number in
+                                Text("\(number)").font(.system(size: 25))
+                            }
+                        } label: { }
+                        Picker(selection: $min) {
+                            ForEach(minValues, id: \.self) { number in
+                                Text("\(number)")
+                                    .font(.system(size: 25))
+                            }
+                        } label: { }
+                        Picker(selection: $sec) {
+                            ForEach(secValues, id: \.self) { number in
+                                Text("\(number)").font(.system(size: 25))
+                            }
+                        } label: { }
                     }
+                    .pickerStyle(.wheel)
+                    .frame(height: 160)
                 
-                HStack {
-                    Picker(selection: $hr) {
-                        ForEach(hrValues, id: \.self) { number in
-                            Text("\(number)").font(.system(size: 25))
-                        }
-                    } label: { }
-                    Picker(selection: $min) {
-                        ForEach(minValues, id: \.self) { number in
-                            Text("\(number)")
-                                .font(.system(size: 25))
-                        }
-                    } label: { }
-                    Picker(selection: $sec) {
-                        ForEach(secValues, id: \.self) { number in
-                            Text("\(number)").font(.system(size: 25))
-                        }
-                    } label: { }
-                }
-                .pickerStyle(.wheel)
-                .frame(height: 160)
-            
-                LazyVGrid(columns: columns) {
-                    ForEach(digits, id:\.self) { symbol in
-                        Button {
-                            
-                        } label: {
-                            Circle().fill(symbol == "✕" ? .red : color)
-                                .overlay {
-                                    Text(symbol)
-                                        .font(font)
-                                        .foregroundColor(white)
-                                }
+                    LazyVGrid(columns: columns) {
+                        ForEach(digits, id:\.self) { symbol in
+                            Button {
+                                
+                            } label: {
+                                Circle().fill(symbol == "✕" ? .red : color)
+                                    .overlay {
+                                        Text(symbol)
+                                            .font(font)
+                                            .foregroundColor(white)
+                                    }
+                            }
                         }
                     }
                 }
+                .padding(3)
             }
-            .padding(3)
+            Rectangle()
+                .fill(Color.clear)
+                .frame(width: 40)
+                .contentShape(Rectangle()) //use if color clear otherwise gesture will not work
+                .ignoresSafeArea()
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                    .onEnded { _ in
+                        withAnimation {
+                            withAnimation {
+                                viewModel.durationPicker_OfColor = nil
+                            }
+                        }
+                    }
+                )
         }
         .ignoresSafeArea()
     }
