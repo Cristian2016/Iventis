f5cba319eae6902186b848c701101c6d5470a6c0
Cristian <Cristian>
2023-02-06 19:28:45 +0200

    <#key#>

--- MoreOptionsView1.swift
+++ MoreOptionsView1.swift
@@ -6,15 +6,24 @@
 //
 
 import SwiftUI
+import MyPackage
 
 struct MoreOptionsView1: View {
     let bubble:Bubble
     @State private var startDelay:Int64?
+    let metrics = Metrics()
     
     var body: some View {
         ZStack {
-            
+            BlurryBackground(material: .thinMaterial)
+            Color.white.cornerRadius(metrics.radius)
+                .padding()
+                .padding()
         }
+    }
+    
+    struct Metrics {
+        let radius = CGFloat(20)
     }
 }
 
