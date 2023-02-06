58e1273698af41674ac3cc9f0fc620ecf36ae1e6
Cristian <Cristian>
2023-02-06 19:52:59 +0200

    ok

--- MoreOptionsView1.swift
+++ MoreOptionsView1.swift
@@ -18,6 +18,7 @@ struct MoreOptionsView1: View {
         GeometryReader { geo in
             let isPortrait = geo.size.height > geo.size.width
             let layout = isPortrait ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+            let bubbleColor = Color.bubbleColor(forName: bubble.color)
             
             ZStack {
                 BlurryBackground(material: .ultraThinMaterial)
@@ -26,16 +27,34 @@ struct MoreOptionsView1: View {
                     .standardShadow()
                     .overlay {
                         layout {
+                            HStack(spacing: 4) {
+                                ForEach(Bubble.delays, id:\.self) { delay in
+                                    Button {
+                                        
+                                    } label: {
+                                        bubbleColor
+                                            .aspectRatio(4/3, contentMode: .fit)
+                                            .overlay {
+                                                Text(String(delay))
+                                                    .foregroundColor(.white)
+                                                    .font(Font.system(size: 30).weight(.medium))
+                                            }
+                                    }
+                                }
+                            }
+                            Divider()
                             ScrollView {
-                                LazyVGrid(columns: metrics.columns) {
+                                LazyVGrid(columns: metrics.columns, spacing: 4) {
                                     ForEach(Color.triColors) { tricolor in
                                         ZStack {
                                             Circle()
                                             tricolor.sec
                                         }
+                                        .aspectRatio(isPortrait ? 3/2 : 3/1, contentMode: .fit)
                                     }
                                 }
                             }
+                            .frame(minWidth: 330)
                             .scrollIndicators(.hidden)
                         }
                         .padding(10)
