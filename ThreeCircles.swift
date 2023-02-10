740fbecb2d665d9d00df73188094700983c2b02a
Cristian <Cristian>
2023-02-10 10:37:12 +0200

    ok

---
+++ ThreeCircles.swift
@@ -0,0 +1,85 @@
+//
+//  ThreeCircles.swift
+//  Timers (iOS)
+//
+//  Created by Cristian Lapusan on 10.02.2023.
+// it shows bubble.color and shows or hide minutes and hours bubble. that's it!
+
+import SwiftUI
+import Combine
+import MyPackage
+
+struct ThreeCircles: View {
+    let coordinator = BubbleCellCoordinator()
+    
+    let spacing = CGFloat(-40)
+    let color = Color.red
+    
+    @State private var showMin = false
+    @State private var showHr = false
+    
+    var body: some View {
+        VStack {
+            Button("Ok") {
+                coordinator.publisher.send(.min(showMin ? false : true))
+            }
+            HStack(spacing: spacing) {
+                circle(.hr)
+                circle(.min)
+                circle(.sec)
+            }
+        }
+        .onReceive(coordinator.publisher) {
+            switch $0 {
+                case .none: break
+                case .min(let show):
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.6)) { showMin = show }
+                case .hr(let show):
+                    withAnimation(.spring(response: 0.8, dampingFraction: 0.2)) { showHr = show }
+            }
+        }
+    }
+    
+    // MARK: - Lego
+    
+    @ViewBuilder
+    private func circle(_ kind:Kind) -> some View {
+        switch kind {
+            case .hr:
+                Circle()
+                    .fill(color)
+                    .scaleEffect(showHr ? 1 : 0)
+            case .min:
+                Circle()
+                    .fill(color)
+                    .scaleEffect(showMin ? 1 : 0)
+            case .sec:
+                Circle()
+                    .fill(color)
+        }
+    }
+    
+    // MARK: -
+    enum Kind {
+        case sec
+        case min
+        case hr
+    }
+}
+
+class BubbleCellCoordinator: ObservableObject {
+    var publisher:CurrentValueSubject<Show, Never> = .init(.none)
+    
+    private var show = Show.none
+    enum Show {
+        case min(Bool)
+        case hr(Bool)
+        case none
+    }
+}
+
+struct ThreeCircles_Previews: PreviewProvider {
+    static var previews: some View {
+        ThreeCircles()
+    }
+}
---
+++ tBubbleCell.swift
@@ -0,0 +1,62 @@
+//
+//  tBubbleCell.swift
+//  Timers (iOS)
+//
+//  Created by Cristian Lapusan on 09.02.2023.
+//
+
+import SwiftUI
+
+struct tBubbleCell: View {
+    private let secretary = Secretary.shared
+    
+    @ObservedObject var bubble:Bubble
+    
+    @EnvironmentObject private var viewModel:ViewModel
+    @EnvironmentObject private var layoutViewModel:LayoutViewModel
+    
+    init(_ bubble: Bubble, _ metrics: Metrics) {
+        self.bubble = bubble
+    }
+    
+    var body: some View {
+        let _ = print("tBubble body")
+        bubbleCellShape(Color.bubbleColor(forName: bubble.color))
+            .frame(height: 100)
+            .onTapGesture {
+                bubble.color = Color.triColors.randomElement()!.description
+                PersistenceController.shared.save()
+            }
+    }
+    
+    // MARK: - Lego
+    @ViewBuilder
+    private func  bubbleCellShape(_ color:Color) -> some View {
+        if bubble.hasWidget {
+            color.cornerRadius(20)
+        } else {
+            Circle().fill(color)
+        }
+    }
+}
+
+
+
+extension tBubbleCell {
+    ///circle diameter, font size, spacing and so on
+    struct Metrics {
+        static var width:CGFloat = 0
+        
+        init(_ width:CGFloat) {
+            self.spacing = width * -0.18
+            self.timeComponentsFontSize = width * CGFloat(0.16)
+            self.hundredthsFontSize = width * CGFloat(0.06)
+            Metrics.width = width
+        }
+        
+        let spacing:CGFloat
+        let timeComponentsFontSize:CGFloat
+        let hundredthsFontSize:CGFloat
+        let cellPadding = EdgeInsets(top: 0, leading: -12, bottom: 0, trailing: -12)
+    }
+}
