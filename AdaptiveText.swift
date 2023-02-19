06ecbebf376ab3b10da751348b6d96d6377caf17
Cristian <Cristian>
2023-02-19 11:07:59 +0200

    ok

--- AdaptiveText.swift
+++ AdaptiveText.swift
@@ -29,24 +29,6 @@ struct AdaptiveText: ViewModifier {
                 }
         }
     }
-    
-//    private var hundredthsView:some View {
-//        Push(.bottomRight) {
-//            Text(hundredths)
-//                .padding()
-//                .background(Circle().foregroundColor(.pauseStickerColor))
-//                .foregroundColor(.pauseStickerFontColor)
-//                .font(.system(size: hundredthsFontSize, weight: .semibold, design: .rounded))
-//                .scaleEffect(isSecondsTapped && !isBubbleRunning ? 2 : 1.0)
-//                .offset(x: isSecondsTapped && !isBubbleRunning ? -20 : 0,
-//                        y: isSecondsTapped && !isBubbleRunning ? -20 : 0)
-//                .opacity(isSecondsTapped && !isBubbleRunning ? 0 : 1)
-//                .animation(.spring(response: 0.3, dampingFraction: 0.2), value: isSecondsTapped)
-//                .zIndex(1)
-//                .onTapGesture { userTappedHundredths() }
-//                .onReceive(bubble.coordinator.$components) { hundredths = $0.hundredths }
-//        }
-//    }
 }
 
 extension View {
