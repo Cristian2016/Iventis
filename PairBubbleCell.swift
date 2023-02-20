3fc8896d52c24f873ed030173a7306ee2bca4a3f
Cristian <Cristian>
2023-02-20 08:22:30 +0200

    ok

--- PairBubbleCell.swift
+++ PairBubbleCell.swift
@@ -10,35 +10,7 @@ import MyPackage
 
 ///it's the small bubble cell in the PairCell of BottomDetaiulView that only shows up when bubble is running and detailMode is active
 struct PairBubbleCell: View {
-    enum Appearance { //4 cases
-        case bigBlackBackground //default
-        case bigWhiteBackground
-    }
-    
-    ///user taps and cycles through various looks
-    var appearance: Appearance {
-        get {
-            switch skinTapsCount%2 {
-                case 0: return .bigBlackBackground
-                case 1: return .bigWhiteBackground
-                default: return .bigBlackBackground
-            }
-        }
-        
-        set {
-            switch newValue {
-                case .bigBlackBackground : skinTapsCount = 0
-                case .bigWhiteBackground : skinTapsCount = 1
-            }
-        }
-    }
-    
-    ///user taps and chooses various skins
-    @AppStorage("skinTapsCount") var skinTapsCount: Int = 0
-    @AppStorage("zoom") var isZoomed: Bool = true
-    
-    @Environment(\.colorScheme) var colorScheme
-    
+            
     @StateObject var bubble:Bubble
     let metrics:BubbleCell.Metrics
     
@@ -47,10 +19,6 @@ struct PairBubbleCell: View {
             background
             timeComponents
                 .foregroundColor(Color("smallBubbleCircleColor"))
-                .onTapGesture { withAnimation {
-                    isZoomed.toggle()
-                    skinTapsCount += 1
-                } }
         }
     }
     
@@ -59,33 +27,26 @@ struct PairBubbleCell: View {
     
     // MARK: - LEGOS
     var background: some View {
-        HStack (spacing: -40) {
-            //Hours
-            Circle().opacity(hrOpacity)
-            //Minutes
-            Circle().opacity(minOpacity)
-            //Seconds
-            Circle()
-        }
-        .compositingGroup()
-        .foregroundColor(.white)
-        .standardShadow()
-        .background {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(fillColor)
-                .padding(-10)
-        }
+        Rectangle().fill(.clear)
+            .aspectRatio(metrics.ratio, contentMode: .fit)
+            .overlay {
+                HStack {
+                    whiteCircle
+                    whiteCircle
+                    whiteCircle
+                }
+                .scaleEffect(x: metrics.hstackScale, y: metrics.hstackScale)
+            }
+            .background {
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.label)
+            }
     }
     
-    var fillColor: Color {
-        let isLight = colorScheme == .light
-        
-        switch appearance {
-            case .bigBlackBackground:
-                return isLight ? .black : .clear
-            default:
-                return .clear
-        }
+    private var whiteCircle:some View {
+        Circle()
+            .fill(.white)
+            .scaleEffect(x: metrics.circleScale, y: metrics.circleScale)
     }
     
     var timeComponents: some View {
@@ -93,12 +54,10 @@ struct PairBubbleCell: View {
             //HOURS
             Circle().fill(Color.clear)
                 .overlay { Text(bubble.smallBubbleView_Components.hr) }
-                .opacity(hrOpacity)
             
             //MINUTES
             Circle().fill(Color.clear)
                 .overlay { Text(bubble.smallBubbleView_Components.min) }
-                .opacity(minOpacity)
             
             //SECONDS
             ZStack {
@@ -110,16 +69,4 @@ struct PairBubbleCell: View {
         .fontDesign(.rounded)
         .foregroundColor(.black)
     }
-    
-    //stopwatch: minutes and hours stay hidden initially
-    private var minOpacity:Double {
-        bubble.smallBubbleView_Components.min > "0" || bubble.smallBubbleView_Components.hr > "0" ? 1 : 0.001
-    }
-    private var hrOpacity:Double { bubble.smallBubbleView_Components.hr > "0" ? 1 : 0.001 }
 }
-
-//struct PairRunningCell_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SmallBubbleCell(bubble: <#Bubble#>)
-//    }
-//}
