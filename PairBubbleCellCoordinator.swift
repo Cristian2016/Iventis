4d73afc57ad04210ed864a159c905573e8ae55bf
Cristian <Cristian>
2023-02-20 09:48:55 +0200

    ok

--- PairBubbleCellCoordinator.swift
+++ PairBubbleCellCoordinator.swift
@@ -3,14 +3,41 @@
 //  Timers (iOS)
 //
 //  Created by Cristian Lapusan on 20.02.2023.
-//
+//1 if cancellable is set to emty set, it will stop updating
 
-import SwiftUI
+import Combine
 
 class PairBubbleCellCoordinator {
     unowned private let bubble:Bubble
     
+    @Published private(set) var components = Components("-1", "-1", "-1")
+    
     init(bubble: Bubble) {
         self.bubble = bubble
+    }
+    
+    private func update(_ action:Action) {
+        
+    }
+    
+    private var cancellable = Set<AnyCancellable>() //1
+}
+
+extension PairBubbleCellCoordinator {
+    struct Components {
+        var hr:String
+        var min:String
+        var sec:String
+        
+        init(_ hr: String, _ min: String, _ sec: String) {
+            self.hr = hr
+            self.min = min
+            self.sec = sec
+        }
+    }
+    
+    enum Action {
+        case start
+        case pause
     }
 }
