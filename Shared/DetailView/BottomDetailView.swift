6eaab72ab1bbc9a165c057d70de58773f378b937
Cristian <Cristian>
2023-02-23 16:52:12 +0200

    ok

--- Shared/DetailView/BottomDetailView.swift
+++ Shared/DetailView/BottomDetailView.swift
@@ -11,18 +11,20 @@ import SwiftUI
 struct BottomDetailView: View {
     @FetchRequest var sessions:FetchedResults<Session>
     @State private var pairBubbleCellNeedsDisplay = false
-    private let bubble:Bubble
+    private let coordinator:BubbleCellCoordinator
     
     private let secretary = Secretary.shared
     @State private var selectedTab = 0
     
     init?(_ bubble:Bubble?) {
         guard let bubble = bubble else { return nil }
-        self.bubble = bubble
+        
+        self.coordinator = bubble.coordinator
         
         let predicate = NSPredicate(format: "bubble.rank == %i", bubble.rank)
         let descriptor = NSSortDescriptor(key: "created", ascending: false)
         _sessions = FetchRequest(entity: Session.entity(), sortDescriptors: [descriptor], predicate: predicate, animation: .easeInOut)
+        
     }
     
     var body: some View {
@@ -35,12 +37,13 @@ struct BottomDetailView: View {
         .padding(.init(top: 0, leading: -12, bottom: 0, trailing: -12))
         .tabViewStyle(.page(indexDisplayMode: .never))
         .onReceive(secretary.$pairBubbleCellNeedsDisplay) { pairBubbleCellNeedsDisplay = $0 }
-//        .onReceive(bubble.coordinator.$needleRank) { needleRank in
-//            if !bubble.coordinator.userMovedNeedle {
-//                withAnimation { self.selectedTab = needleRank ?? 0 }
-//            }
+        .onReceive(coordinator.$needleRank) { needleRank in
+           
+        }
+//        .onChange(of: selectedTab) {
+//            bubble.coordinator.needleRank = $0
+//            print("onchange called")
 //        }
-        .onChange(of: selectedTab) { bubble.coordinator.needleRank = $0 }
     }
     
     private func sessionRank(of session:Session) -> Int {
