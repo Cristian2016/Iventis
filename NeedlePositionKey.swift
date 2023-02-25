d9eba45f7ffade0ebdde38627a4535b38b9d41f9
Cristian <Cristian>
2023-02-25 09:22:12 +0200

    ok

--- NeedlePositionKey.swift
+++ NeedlePositionKey.swift
@@ -10,3 +10,16 @@ import SwiftUI
 struct NeedlePositionKey : EnvironmentKey {
     static var defaultValue:Binding<Int> = .constant(-1)
 }
+
+extension EnvironmentValues {
+    public var needlePosition: Binding<Int> {
+        get { self[NeedlePositionKey.self] }
+        set { self[NeedlePositionKey.self] = newValue }
+    }
+}
+
+extension View {
+    func needlePosition(_ value:Binding<Int>) -> some View {
+        self.environment(\.needlePosition, value)
+    }
+}
