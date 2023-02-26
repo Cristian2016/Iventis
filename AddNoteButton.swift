775ea70081338b41cea12380be6d0b3101207f0e
Cristian <Cristian>
2023-02-26 13:47:21 +0200

    ok

--- AddNoteButton.swift
+++ AddNoteButton.swift
@@ -3,7 +3,6 @@
 //  Timers (iOS)
 //
 //  Created by Cristian Lapusan on 01.02.2023.
-//⚠️ task { } called only once! when app is launched. no idea why is not called each time self appears
 
 import SwiftUI
 import MyPackage
@@ -33,7 +32,7 @@ struct AddNoteButton: View {
                                               isFilled: true)
                     )
                 }
-            } else { EmptyView() }
+            }
         }
         .onReceive(secretary.$addNoteButton_bRank) { addNoteButton_bRank = $0 }
     }
