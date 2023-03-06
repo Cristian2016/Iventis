51f421bd548b138593d6fea248aa35da2161c7f0
Cristian <Cristian>
2023-03-06 14:19:08 +0200

    ok

--- Shared/Core Data/Persistence.swift
+++ Shared/Core Data/Persistence.swift
@@ -77,11 +77,9 @@ struct PersistenceController {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
+        container.loadPersistentStores { description, error in
+            if let error = error as NSError? { fatalError() }
+        }
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
