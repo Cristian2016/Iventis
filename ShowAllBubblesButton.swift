ba5b6b1fc711861b17e621641b1ac0f855cd150e
Cristian <Cristian>
2023-03-09 18:21:13 +0200

    ok

--- ShowAllBubblesButton.swift
+++ ShowAllBubblesButton.swift
@@ -58,7 +58,6 @@ struct ShowAllBubblesButton: View {
                     }
                 }
                 .onTapGesture { showAllBubbles()
-                   
                 }
             }
         }
