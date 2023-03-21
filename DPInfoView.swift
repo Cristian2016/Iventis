e5f34c5976ae7c20c8a701b0e3da477f78c58c67
Cristian <Cristian>
2023-03-21 21:22:06 +0200

    ok

--- DPInfoView.swift
+++ DPInfoView.swift
@@ -17,7 +17,7 @@ struct DPInfoView: View {
             VStack(alignment: .leading, spacing: 10) {
                 VStack(alignment: .leading) {
                     Text("**Create Timer** \(Image.tap) Tap")
-                    Text("*\(Image.roundCheckmark) symbol shows*")
+                    Text("*If \(Image.roundCheckmark) symbol shows*")
                         .foregroundColor(.secondary)
                 }
                 VStack(alignment: .leading) {
