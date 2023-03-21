db3beee43264979ad3e6258ee24e6f306aebb9f2
Cristian <Cristian>
2023-03-21 14:58:06 +0200

    ok

--- DPInfoView.swift
+++ DPInfoView.swift
@@ -9,36 +9,25 @@ import SwiftUI
 
 struct DPInfoView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("*Ex: Set Duration*")
-                .foregroundColor(.secondary)
-            Text("**1 hour** \(Image.tap)Tap 01")
-            Text("**20 min** \(Image.tap)Tap 00 20")
-            Text("**5 min** \(Image.tap)Tap 00 05")
-            Text("**50 sec** \(Image.tap)Tap 00 00 50")
-            Text("*Tap any Yellow Area to create the timer*")
-                .foregroundColor(.secondary)
-            
-            HStack(alignment: .top) {
-                Image.dpv
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 200)
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("***Yellow Gesture Areas***")
+        HStack(alignment: .top) {
+            Image.dpv
+                .resizable()
+                .scaledToFit()
+                .frame(height: 200)
+            VStack(alignment: .leading, spacing: 4) {
+                Text("***Yellow Gesture Areas***")
+                    .foregroundColor(.secondary)
+                VStack(alignment: .leading) {
+                    Text("**Dismiss** \(Image.tap) Tap")
+                    Text("*If Duration not set*")
                         .foregroundColor(.secondary)
-                    VStack(alignment: .leading) {
-                        Text("**Dismiss** \(Image.tap) Tap")
-                        Text("*If Duration not set*")
-                            .foregroundColor(.secondary)
-                    }
-                    VStack(alignment: .leading) {
-                        Text("**Create Timer** \(Image.tap) Tap")
-                        Text("*If \(Image.roundCheckmark) symbol shows*")
-                            .foregroundColor(.secondary)
-                    }
-                    Text("**Clear Display** \(Image.leftSwipe) Swipe")
                 }
+                VStack(alignment: .leading) {
+                    Text("**Create Timer** \(Image.tap) Tap")
+                    Text("*If \(Image.roundCheckmark) symbol shows*")
+                        .foregroundColor(.secondary)
+                }
+                Text("**Clear Display** \(Image.leftSwipe) Swipe")
             }
         }
         .font(.system(size: 20))
