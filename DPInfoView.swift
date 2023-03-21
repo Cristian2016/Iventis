da1932747faee954f6dcdf2fe1c12391ecb60963
Cristian <Cristian>
2023-03-21 21:27:17 +0200

    ok

--- DPInfoView.swift
+++ DPInfoView.swift
@@ -27,7 +27,7 @@ struct DPInfoView: View {
                 }
                 VStack(alignment: .leading) {
                     Text("**Dismiss** \(Image.tap) Tap")
-                    Text("*If display is clear(ed)*")
+                    Text("*If \(Image.roundCheckmark) does not show*")
                         .foregroundColor(.secondary)
                 }
             }
