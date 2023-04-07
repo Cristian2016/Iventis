2bc8e274e1988f110ca58f51896b3525217d49b2
Cristian <Cristian>
2023-04-07 21:29:19 +0300

    ok

--- InfoOverlay.swift
+++ InfoOverlay.swift
@@ -40,10 +40,10 @@ extension InfoOverlay {
             let symbolTitle:String
             var detail:LocalizedStringKey?
             
-            static let bubbleDelete = Content.init(title: "Dismiss", symbol: "tap", symbolTitle: "Tap", detail: "*outside Gray Shape*")
-            static let createTimer = Content.init(title: "Create Timer", symbol: "tap", symbolTitle: "Tap", detail: "*if \(Image.roundCheckmark) symbol shows*")
-            static let remove = Content.init(title: "Remove", symbol: "leftSwipe", symbolTitle: "Swipe", detail: "*in any direction*")
-            static let dismiss = Content.init(title: "Dismiss", symbol: "tap", symbolTitle: "Tap", detail: "*if \(Image.roundCheckmark) symbol hidden*")
+            static let bubbleDelete = Self.init(title: "Dismiss", symbol: "tap", symbolTitle: "Tap", detail: "*outside Gray Shape*")
+            static let createTimer = Self.init(title: "Create Timer", symbol: "tap", symbolTitle: "Tap", detail: "*if \(Image.roundCheckmark) symbol shows*")
+            static let remove = Self.init(title: "Remove", symbol: "leftSwipe", symbolTitle: "Swipe", detail: "*in any direction*")
+            static let dismiss = Self.init(title: "Dismiss", symbol: "tap", symbolTitle: "Tap", detail: "*if \(Image.roundCheckmark) symbol hidden*")
         }
     }
 }
