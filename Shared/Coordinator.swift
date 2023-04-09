436d5aea72790fd299312050ba1475f3e20a748e
Cristian <Cristian>
2023-04-09 12:28:28 +0300

    ok

--- Shared/Coordinator.swift
+++ Shared/Coordinator.swift
@@ -49,7 +49,7 @@ class BubbleCellCoordinator {
                     self.cancellable = []
                     DispatchQueue.main.async {
                         self.timeComponents = Components("0", "0", "0", "✕")
-                        self.timerProgress = "1.0"
+                        self.timerProgress = "Done"
                     }
                     
                 case .showAll:
@@ -62,7 +62,12 @@ class BubbleCellCoordinator {
                     DispatchQueue.main.async {
                         self.timeComponents = Components(comp.hr, comp.min, comp.sec, comp.hundredths)
                         self.timeComponentsOpacity.updateOpacity(theInitialValue)
-                        self.timerProgress = String(format: "%.2f", progress)
+                        
+                        if bubble.state == .finished {
+                            self.timerProgress = "Done"
+                        } else {
+                            self.timerProgress = String(format: "%.2f", progress)
+                        }
                     }
                     
                     if theBubble.state == .running {
@@ -102,7 +107,7 @@ class BubbleCellCoordinator {
                             let stringComponents = theBubble.initialClock.timeComponentsAsStrings
                             
                             DispatchQueue.main.async {
-                                self.timerProgress = "0.0"
+                                self.timerProgress = "0.00"
                                 
                                 self.timeComponents.hr = stringComponents.hr
                                 self.timeComponents.min = stringComponents.min
@@ -123,7 +128,7 @@ class BubbleCellCoordinator {
     @Published var sdbOffset = CGFloat(0) //start delay button YOffset
     @Published var sdbDeleteTriggered = false //start delay button delete triggered
     
-    @Published var timerProgress = "0.0" //8
+    @Published var timerProgress = "0.00" //8
     @Published var timeComponents = Components("-1", "-1", "-1", "-1")
     var timeComponentsSet:Bool { timeComponents.hr != "-1" }
     @Published private(set) var timeComponentsOpacity = Opacity()
@@ -151,7 +156,9 @@ class BubbleCellCoordinator {
         if isTimer {
             //compute progress
             let progress = self.computeTimerProgress(for: bubble, and: value)
-            DispatchQueue.main.async { self.timerProgress = String(format: "%.2f", progress) }
+            DispatchQueue.main.async {
+                self.timerProgress = (self.timerProgress != "1.00") ? String(format: "%.2f", progress) : "Done"
+            }
             
             //check if timer should finish
             let totalDuration = bubble.lastSession!.totalDuration
