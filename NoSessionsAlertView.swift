0fd455b9fc59345f53472667674898457f6e752b
Cristian <Cristian>
2023-04-14 20:22:51 +0300

    ok

--- NoSessionsAlertView.swift
+++ NoSessionsAlertView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct NoSessionsAlertView: View {
     var body: some View {
         VStack (alignment:.leading) {
-            Text("No History").font(.title)
+            Text("No Entries").font(.title)
             Text("**Start** \(Image.tap) Tap Seconds")
                 .foregroundColor(.secondary)
         }
