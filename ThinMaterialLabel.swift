b4ae81d07662a2bc0e105f51b78ce4ae5393027a
Cristian <Cristian>
2023-04-15 23:47:15 +0300

    ok

--- ThinMaterialLabel.swift
+++ ThinMaterialLabel.swift
@@ -57,7 +57,7 @@ struct ThinMaterialLabel<Content:View>: View {
     
     private var dismissButton:some View {
         Button { action() } label: {
-            Text("Dismiss").frame(maxWidth: .infinity)
+            Text("OK").frame(maxWidth: .infinity)
         }
         .tint(.red)
         .buttonStyle(.borderedProminent)
