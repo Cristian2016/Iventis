62178e33a46fb3ac2a9a3798dcbe19e9661a78eb
Cristian <Cristian>
2023-04-24 17:36:37 +0300

    ok

--- Shared/Duration Picker/DurationPickerView.swift
+++ Shared/Duration Picker/DurationPickerView.swift
@@ -162,7 +162,7 @@ extension DurationPickerView {
     struct Info:View {
         @State private var show = false
         private let title = "Choose Duration"
-        private let subtitle:LocalizedStringKey = "\(Image(systemName: "checkmark.circle.fill")) green checkmark means 'chosen duration is valid'"
+        private let subtitle:LocalizedStringKey = "a \(Image(systemName: "checkmark.circle.fill")) checkmark in green confirms the entered duration is valid"
         
         var body: some View {
             ZStack {
