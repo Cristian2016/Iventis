20f396534104ece2067670b83bc24acb66f791fc
Cristian <Cristian>
2023-04-25 11:42:49 +0300

    ok

--- InfoCell.swift
+++ InfoCell.swift
@@ -37,6 +37,7 @@ struct InfoCell: View {
                         Image(imageName)
                             .resizable()
                             .scaledToFit()
+                            .frame(maxHeight: 200)
                     }
                 }
             case .small:
