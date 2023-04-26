ce14828fb3cb57daf116f35c0703e03a57544632
Cristian <Cristian>
2023-04-26 15:10:55 +0300

    okok

--- InfoOutlineUnit.swift
+++ InfoOutlineUnit.swift
@@ -53,6 +53,7 @@ extension InfoOutlineUnit {
         static let enableCal3 = Input(title: "A calendar event will be created for each closed entry. To close an entry *\(Image.longPress) long-press* on seconds", image: "event.entry")
         
         static let aepActivity = Input(title: "A bubble's activity (activity log) represents the usage of that bubble over time. The activity is made up of entries", image: "bubbleActivity", footnote: "*\(Image.tap) Tap* a bubble's hours or minutes area to view its activity")
+        static let aepEntry = Input(title: "", image: "entry")
     }
 }
 
