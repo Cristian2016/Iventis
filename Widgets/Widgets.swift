cf5848d2700ffd9f59cafbcbf70806b84b1c102f
Cristian <Cristian>
2023-04-27 13:27:21 +0300

    ok

--- Widgets/Widgets.swift
+++ Widgets/Widgets.swift
@@ -50,7 +50,7 @@ struct Widgets: Widget {
             WidgetsEntryView(entry: entry)
         }
         .configurationDisplayName("Recent Bubble")
-        .description("Most recently used bubble")
+        .description("Observe the most recently used bubble")
         .supportedFamilies([.accessoryCircular, .accessoryInline])
     }
 }
