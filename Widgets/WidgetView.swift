c2ec3659b17227422d81ab87f3d31d202672d867
Cristian <Cristian>
2023-04-28 22:28:52 +0300

    ok

--- Widgets/WidgetView.swift
+++ Widgets/WidgetView.swift
@@ -20,7 +20,7 @@ struct WidgetView : View {
                     .aspectRatio(2.2, contentMode: .fit)
                     .overlay {
                         display
-                            .padding([.leading, .trailing], 2)
+                            .padding([.leading, .trailing], 4)
                             .font(.title3)
                             .minimumScaleFactor(0.01)
                             .multilineTextAlignment(.center)
