829f6f8d13ff6aae80f53ae52940b39998dc9b96
Cristian <Cristian>
2023-05-06 08:04:08 +0300

    ok

--- Cell.swift
+++ Cell.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct Cell: View {
+    
+    @ObservedObject var bubble:Bubble
+    @EnvironmentObject private var viewModel:ViewModel
+    private let secretary = Secretary.shared
+    
     var body: some View {
         Rectangle()
             .fill(.clear)
@@ -15,20 +20,64 @@ struct Cell: View {
             .overlay {
                 ZStack {
                     HStack {
-                        Circle()
-                            .scale(x: 0.94, y: 0.94)
-                        Circle()
-                            .scale(x: 0.94, y: 0.94)
+                        circle
+                        circle
                     }
-                    Circle()
-                        .scale(x: 0.94, y: 0.94)
+                   circle
                 }
             }
+//            .swipeActions(edge: .leading, allowsFullSwipe: true) {
+//                toggleFavoriteButton
+//                toggleCalendarButton
+//            }
+//            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+//                deleteActionButton
+//                moreOptionsButton
+//            }
+    }
+    
+    private var circle:some View {
+        Circle()
+            .scale(x: 0.98, y: 0.98)
+    }
+    
+    //Leading Swipe actions
+    private var toggleFavoriteButton:some View {
+        Button { viewModel.togglePin(bubble) }
+    label: { Label { Text(bubble.isPinned ? "Unpin" : "Pin") }
+        icon: { Image(systemName: bubble.isPinned ? "pin.slash.fill" : "pin.fill") } }
+    .tint(bubble.isPinned ? .gray : .orange)
+    }
+    
+    private var toggleCalendarButton:some View {
+        Button { viewModel.toggleCalendar(bubble) }
+    label: { Label { Text(calendarActionName) }
+        icon: { Image(systemName: calendarActionImageName) } }
+    .tint(calendarActionColor)
+    }
+    
+    //trailing Swipe actions
+    private var deleteActionButton:some View {
+        Button { secretary.deleteAction_bRank = bubble.rank }
+        label: { Label { Text("Delete") } icon: { Image.trash } }.tint(.red) }
+    
+    private var moreOptionsButton:some View {
+        Button { viewModel.showMoreOptions(for: bubble) }
+    label: { Label { Text("More") } icon: { Image.more } }.tint(.lightGray)
     }
 }
 
-struct Cell_Previews: PreviewProvider {
-    static var previews: some View {
-        Cell()
-    }
+extension Cell {
+    // MARK: - Helpers
+    private var calendarActionName:String { bubble.hasCalendar ? "Cal OFF" : "Cal ON" }
+    
+    private var calendarActionImageName:String { bubble.hasCalendar ? "calendar" : "calendar" }
+    
+    private var calendarActionColor:Color { bubble.hasCalendar ? Color.calendarOff : .calendar }
 }
+
+//struct Cell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Cell()
+//    }
+//}
