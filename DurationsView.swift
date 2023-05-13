c21b2a7a258b4d8106acebd8b6aae802b6d3dd1b
Cristian <Cristian>
2023-05-13 15:23:22 +0300

    okok

--- DurationsView.swift
+++ DurationsView.swift
@@ -23,7 +23,7 @@ struct DurationsView: View {
     var body: some View {
         ZStack {
                 VStack(spacing: 0) {
-                    Text("*\(Image.timer) Minutes*")
+                    Text("*\(Image.timer) Choose Minutes*")
                         .padding(10)
                         .font(.system(size: 22))
                         .foregroundColor(.black)
