0a12697a3becef94de7df516da821d12c4823e85
Cristian <Cristian>
2023-05-29 19:17:40 +0300

    ok

--- Shared/Duration Picker/DPDisplay.swift
+++ Shared/Duration Picker/DPDisplay.swift
@@ -59,7 +59,7 @@ extension DurationPickerView {
         private var explainView:some View {
             if manager.digits.isEmpty {
                 HStack {
-                    Text("48 hr max")
+                    Text("48 hours max")
                         .font(.system(size: 20, weight: .medium))
                     Divider().frame(height: 14)
                 }
