99b6d57fe27dd1c5afeab211e0801ae9f060136d
Cristian <Cristian>
2023-06-03 19:17:46 +0300

    ok

--- WarningLabel.swift
+++ WarningLabel.swift
@@ -14,26 +14,34 @@ struct WarningLabel: View {
     var body: some View {
         ZStack {
             if show {
-                VStack(spacing: 8) {
-                    Image(systemName: "calendar.badge.exclamationmark")
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.red, Color.label)
-                        .font(.system(size: 40))
-                    Text("Calendar Access Denied")
-                        .font(.system(size: 30))
-                    Divider()
-                    Text("Settings App > Fused > Calendars")
-                        .font(.system(size: 22))
-                    Text("*Open 'Settings App' on your device > Scroll down to 'Fused' and tap it > Make sure 'Calendars' Toggle is ON*")
-                        .font(.system(size: 18))
-                        .foregroundColor(.secondary)
-                    Image("calToggle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-                .padding()
-                .frame(width: 360)
+                ZStack {
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            Secretary.shared.showCalendarAccessDeniedWarning = false
+                        }
+                    
+                    VStack(spacing: 8) {
+                        Image(systemName: "calendar.badge.exclamationmark")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(.red, Color.label)
+                            .font(.system(size: 40))
+                        Text("Calendar Access Denied")
+                            .font(.system(size: 30))
+                        Divider()
+                        Text("Settings App > Fused > Calendars")
+                            .font(.system(size: 22))
+                        Text("*Open 'Settings App' on your device > Scroll down to 'Fused' and tap it > Make sure 'Calendars' Toggle is ON*")
+                            .font(.system(size: 18))
+                            .foregroundColor(.secondary)
+                        Image("calToggle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .padding()
+                    .frame(width: 360)
                 .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 20))
+                }
             }
         }
         .onReceive(Secretary.shared.$showCalendarAccessDeniedWarning) { show = $0 }
