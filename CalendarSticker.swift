18ed4fd5eef80f91b7ccd8f57498a953d7473261
Cristian <Cristian>
2023-06-04 18:27:12 +0300

    ok

--- CalendarSticker.swift
+++ CalendarSticker.swift
@@ -6,19 +6,16 @@
 //
 
 import SwiftUI
+import MyPackage
 
 struct CalendarSticker: View {
-    let calRatio:CGFloat = 98.0/91.0
-    
     var body: some View {
-        ZStack {
-            let calAccessGranted = CalendarManager.shared.calendarAccessStatus == .granted
-            let imageName = calAccessGranted  ? "calendar" : "calendar.badge.exclamationmark"
-            
-            Image(systemName: imageName)
-                .foregroundColor(.calendar)
-                .font(.system(size: 43))
-        }
+        let calAccessGranted = CalendarManager.shared.calendarAccessStatus == .granted
+        let imageName = calAccessGranted  ? "calendar" : "calendar.badge.exclamationmark"
+        
+        Image(systemName: imageName)
+            .foregroundColor(.calendar)
+            .font(.system(size: 43))
     }
 }
 
