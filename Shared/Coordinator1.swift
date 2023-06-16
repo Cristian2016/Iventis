c83f6535947da57a561d9f97cd7702565ceb5789
Cristian <Cristian>
2023-06-16 19:21:34 +0300

    ok

--- Shared/Coordinator1.swift
+++ Shared/Coordinator1.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class BubbleCellCoordinator1 {
+    weak private(set) var bubble:Bubble? = nil
+    
+    init(bubble: Bubble?) {
+        self.bubble = bubble
+    }
+    
+    // MARK: - Publishers
+    var components = Components("-1", "-1", "-1", "-1")
+}
+
+extension BubbleCellCoordinator1 {
+    struct Components {
+        var hr:String
+        var min:String
+        var sec:String
+        var hundredths:String
+        
+        init(_ hr:String, _ min:String, _ sec:String, _ hundredths:String) {
+            self.hr = hr
+            self.min = min
+            self.sec = sec
+            self.hundredths = hundredths
+        }
+    }
+}
