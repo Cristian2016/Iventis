5ef0b46e36e297d32d34bcb07fd580f81e0adfbb
Cristian <Cristian>
2023-07-08 11:09:20 +0300

    ok

--- MoreOptionsView1.swift
+++ MoreOptionsView1.swift
@@ -1,5 +1,5 @@
 //
-//  what.swift
+//  MoreOptionsView1.swift
 //  Timers (iOS)
 //
 //  Created by Cristian Lapusan on 02.07.2023.
@@ -8,86 +8,114 @@
 import SwiftUI
 import MyPackage
 
-struct SomeView: View {
-    var body: some View {
-        ViewThatFits {
-            PortraitView()
-            LandscapeView()
-        }
-        
-    }
-}
-
-struct PortraitView:View {
+struct MoreOptionsView1: View {
+    //set within .onReceive closure. all the information MoreOptionView needs :)
+    @State private var input:Input?
+    private let secretary = Secretary.shared
+    
     var body: some View {
         ZStack {
-            Rectangle()
-                .background(.ultraThinMaterial)
-                .ignoresSafeArea()
-            
-            VStack {
-                Rectangle()
-                    .fill(.white)
-                    .frame(height: 200)
-                
-                UnevenRoundedRectangle(bottomLeadingRadius: 10, bottomTrailingRadius: 10)
-                    .fill(.white)
-                    .overlay {
-                        Grid(horizontalSpacing: 1, verticalSpacing: 1) {
-                            ForEach(Color.paletteTriColors, id: \.self) { tricolors in
-                                GridRow {
-                                    ForEach(tricolors) { tricolor in
-                                        tricolor.sec
-                                    }
-                                }
-                            }
-                        }
-                        .padding(8)
-                    }
-                    .frame(minHeight: 400)
+            if let emptyStruct = input {
+                ViewThatFits {
+                    PortraitView()
+                    LandscapeView()
+                }
             }
-            .padding()
+        }
+        .onReceive(secretary.$moreOptionsBuble) {
+            if let bubble = $0 {
+                let color = Color.bubbleColor(forName: bubble.color)
+                let initialStartDelay = Int(bubble.startDelayBubble?.initialClock ?? 0)
+                                                
+                input = Input(bubble: bubble,
+                              initialBubbleColor: color,
+                              initialStartDelay: initialStartDelay,
+                              userEditedDelay: initialStartDelay)
+                
+            } else { input = nil }
         }
     }
 }
 
-struct LandscapeView:View {
-    var body: some View {
-        ZStack {
-            Rectangle()
-                .ignoresSafeArea()
-                .background(.ultraThinMaterial)
-            
-            HStack(alignment: .top) {
+extension MoreOptionsView1 {
+    struct Input {
+        var bubble:Bubble
+        var initialBubbleColor:Color
+        var initialStartDelay:Int
+        var userEditedDelay:Int
+    }
+    
+    struct PortraitView:View {
+        var body: some View {
+            ZStack {
                 Rectangle()
-                    .fill(.white)
-                    .frame(height: 200)
-                    .frame(minWidth: 300)
-
-                UnevenRoundedRectangle(bottomLeadingRadius: 10, bottomTrailingRadius: 10)
-                    .fill(.white)
-                    .overlay {
-                        ScrollView {
+                    .background(.ultraThinMaterial)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(height: 200)
+                    
+                    UnevenRoundedRectangle(bottomLeadingRadius: 10, bottomTrailingRadius: 10)
+                        .fill(.white)
+                        .overlay {
                             Grid(horizontalSpacing: 1, verticalSpacing: 1) {
                                 ForEach(Color.paletteTriColors, id: \.self) { tricolors in
                                     GridRow {
                                         ForEach(tricolors) { tricolor in
                                             tricolor.sec
-                                                .aspectRatio(2.0, contentMode: .fit)
                                         }
                                     }
                                 }
                             }
+                            .padding(8)
                         }
-                        .padding(8)
-                        .scrollIndicators(.hidden)
-                    }
+                        .frame(minHeight: 400)
+                }
+                .padding()
             }
-            .padding()
+        }
+    }
+
+    struct LandscapeView:View {
+        var body: some View {
+            ZStack {
+                Rectangle()
+                    .ignoresSafeArea()
+                    .background(.ultraThinMaterial)
+                
+                HStack(alignment: .top) {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(height: 200)
+                        .frame(minWidth: 300)
+
+                    UnevenRoundedRectangle(bottomLeadingRadius: 10, bottomTrailingRadius: 10)
+                        .fill(.white)
+                        .overlay {
+                            ScrollView {
+                                Grid(horizontalSpacing: 1, verticalSpacing: 1) {
+                                    ForEach(Color.paletteTriColors, id: \.self) { tricolors in
+                                        GridRow {
+                                            ForEach(tricolors) { tricolor in
+                                                tricolor.sec
+                                                    .aspectRatio(1.8, contentMode: .fit)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            .padding(8)
+                            .scrollIndicators(.hidden)
+                        }
+                }
+                .padding()
+            }
         }
     }
 }
 
 #Preview {
-    SomeView()
+    MoreOptionsView1()
 }
